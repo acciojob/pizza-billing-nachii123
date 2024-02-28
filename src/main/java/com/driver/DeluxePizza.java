e4d8f04f801614c6322622696a6805feb872734b
unknown <nachiket919156@gmail.com>
2024-02-29 00:09:11 +0530

    pizzaBill

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,16 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+    private int price;
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        addExtraCheese();
+        addExtraToppings();
     }
+//    public int getPrice(){
+//        return this.price;
+//    }
+    // addd extra cheeze
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    boolean extracheeze = false;
+    boolean extratopping = false;
+    boolean extrtakeway = false;
+    boolean GetBill = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price = 300;
+
+            bill="base price "+price;
+        }else {
+            price = 400;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +27,44 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(extracheeze== false) {
+            // your code goes here
+            bill = bill + "Extra cheese added " + 80 + "\n";
+            price += 80;
+        }
+        extracheeze = true;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(extratopping == false) {
+            if (isVeg) {
+                price += 70;
+            } else {
+                price += 120;
+            }
+        }
+        extratopping = true;
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(extrtakeway==false) {
+            price += 50;
+        }
+        extrtakeway = true;
     }
+
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(GetBill == false) {
+            bill = bill + "Total Price " + price + "\n";
+            GetBill = true;
+            return bill;
+        }
+        return  null;
+
     }
 }
